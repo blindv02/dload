55b1a68f079f5ed5da2ad0ffc0656c589ed3d0b3
blindv02 <blindv1@gmail.com>
2022-10-05 14:28:39 -0300

    remove residue

---
+++ Dloader/urls.py
@@ -0,0 +1,32 @@
+"""Dloader URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from distutils import core
+from operator import index
+from unicodedata import name
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+import core.views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', core.views.index, name='home1'),
+    path('home/', core.views.index, name='home2'),
+    path('downloaded/', core.views.downloaded, name='downloaded'),
+    path('done/', core.views.done, name='done'),
+    path('error/', core.views.error, name='error'),
+]
---
+++ core/views.py
@@ -0,0 +1,84 @@
+from cgitb import html
+from multiprocessing import context
+from turtle import title
+from unicodedata import name
+from django.shortcuts import render, HttpResponse, redirect
+from pytube import YouTube
+import os
+from os import path, rename, remove
+from django.http import HttpResponse, HttpResponseNotFound
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def downloaded(request):
+    global url, yt
+    url = request.GET.get('url')
+    y = 'youtube.com/watch?v='
+    
+    if y.lower() not in url:
+        return render(request, 'error.html')
+    
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True, progressive=False)
+    embed_link = url.replace('watch?v=', 'embed/')
+    title = yt.title
+    context = {'video': video,'embed': embed_link,'title': title}
+
+    return render(request, 'downloaded.html', context)
+
+def done(request):
+    global url
+    homedir = os.path.expanduser("~\Downloads")
+    button_type1 = request.POST.get("bttn1")
+    button_type2 = request.POST.get("bttn2")
+    
+    if button_type2 == 'mp3':
+        video = YouTube(url).streams.get_audio_only().download(homedir)
+        base, ext = path.splitext(video)
+        new_file = base + '.mp3'
+        rename(video, new_file)
+        
+        try:    
+            with open(new_file, 'rb') as f:
+                file_data = f.read()
+
+            # sending response 
+            response = HttpResponse(file_data, content_type='audio/mpeg')
+            response['Content-Disposition'] = 'attachment; filename="'+ yt.title +'.mp3"'
+            
+        except IOError:
+            # handle file not exist case here
+            response = HttpResponseNotFound(request, 'error.html')
+            
+        remove(new_file)
+        return response
+    
+    elif button_type1 == 'mp4':
+        video = YouTube(url).streams.filter(file_extension='mp4').get_highest_resolution().download(homedir)
+        base, ext = path.splitext(video)
+        new_file = base + '.mp4'
+        rename(video, new_file)
+        
+        try:    
+            with open(new_file, 'rb') as f:
+                file_data = f.read()
+
+            # sending response 
+            response = HttpResponse(file_data, content_type='video/mp4')
+            response['Content-Disposition'] = 'attachment; filename="'+ yt.title +'.mp4"'
+        
+        except IOError:
+            # handle file not exist case here
+            response = HttpResponseNotFound(request, 'error.html')
+            
+        os.remove(new_file)
+        return response
+    else:
+        return render(request, 'error.html')
+
+def error(request):
+    return render(request, 'error.html')
