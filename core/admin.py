4b05d61120403140432d5a4f6f72f921461500b3
Pablo Mamonegit config --global user.email pmamone1@gmail.comgit commit -m primer <pmamone1@gmail.com>
2022-10-21 20:03:32 -0300

    modelo usuario y admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Usuarios
 
+admin.site.site_header="Dload!"
+admin.site.index_title="Administracion del sitio"
+admin.site.site_title="Dload - Administracion"
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('apellido','nombre','email','password','fecha_alta',)
+    
+    search_fields = ['apellido','email']
+    
 # Register your models here.
+admin.site.register(Usuarios, UsuarioAdmin)
+
