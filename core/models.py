2bc0ad61f1c4c33ac024e7c8fbcd23166ac3482e
blindv02 <blindv1@gmail.com>
2022-10-24 13:48:58 -0300

    error de tipeo

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ class Usuarios(models.Model):
     correo_electronico = models.EmailField()
     fecha_registro = models.DateField.auto_now_add()
     
-class multimedia(models.Model):
+class Multimedia(models.Model):
     nombre = models.ForeignKey(Usuarios, null=True, blank=True, on_delete=models.CASCADE)
     titulo = models.CharField(null=False)
     embed_link = models.URLField()
