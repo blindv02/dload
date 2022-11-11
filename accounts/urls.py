b671a1e087ec73cc35db5dd3bff19bc30d7e3204
LeoJaim <leonardo.jaimovich@gmail.com>
2022-11-11 00:30:57 -0300

    segundo commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('registrar/', views.registrar, name='registrar'),
     path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
+
 ]
